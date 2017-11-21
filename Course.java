97abbfaf58297b737372bf1a65bc03d743e61e1d
spanky3449 <beg12@my.fsu.edu>
2017-11-20 21:02:17 -0500

    Add files via upload
    Some data structs to hold info for our classes with some functions. change as needed

---
+++ Course.java
@@ -0,0 +1,26 @@
+public class Course{
+    private String courseNum;
+    private Group[] groups;
+    private int groupNum;
+
+    public Course(String cn) {
+        courseNum = cn;
+        groups = new Group[5];
+    }
+
+    public void addGroup(Group g) {
+        groups[groupNum++] = g;
+    }
+
+    public String getCourseNum() {
+        return courseNum;
+    }
+
+    public int getGroupNum() {
+        return groupNum;
+    }
+
+    public void removeGroup() {
+        groupNum--;
+    }
+}
